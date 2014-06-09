180dae95b8fd70b1ca7859395de8e6eb8607e975
muoidv <muoidv@smartoc.com>
2014-06-09 22:03:32 +0700

    link login admin

--- ru/wp-login.php
+++ ru/wp-login.php
@@ -876,13 +876,13 @@ default:
 	<p class="submit">
     <input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="<?php esc_attr_e('Log In'); ?>" />
     <div class="language" style="float: left; margin-top: 18px;">
-        <a href="<?php echo get_site_url(); ?>/en/wp-admin" style="margin-right: 10px">
+        <a href="http://zuutheme.com/ifk/wp-admin" style="margin-right: 10px">
             <img src="<?php bloginfo('template_directory'); ?>/images/en-US.png" title="English" alt="en-US.png">English
         </a>
-        <a href="<?php echo get_site_url(); ?>/ru/wp-admin" style="margin-right: 10px">
+        <a href="http://zuutheme.com/ifk/ru/wp-admin" style="margin-right: 10px">
             <img src="<?php bloginfo('template_directory'); ?>/images/ru-RU.png" title="Russia" alt="ru-RU.png">Russia
         </a>
-        <a href="<?php echo get_site_url(); ?>/de/wp-admin" style="margin-right: 10px">
+        <a href="http://zuutheme.com/ifk/de/wp-admin" style="margin-right: 10px">
             <img src="<?php bloginfo('template_directory'); ?>/images/de-DE.png" title="Germany" alt="de-DE.png">German
         </a>
 
