a21d26d1046dafc67dbfee7748a146fe30c0196b
muoidv <muoidv@smartoc.com>
2014-06-09 21:55:11 +0700

    search

--- de/wp-content/themes/ifk/searchform.php
+++ de/wp-content/themes/ifk/searchform.php
@@ -7,7 +7,7 @@
  * @since Twenty Eleven 1.0
  */
 ?>
-	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/de/' ) ); ?>">
 		<input id="keyword" title="keyword" name="s" id="s" class="txt_field" placeholder="Search here" type="text"/>
 		<input type="submit" class="submit sub_btn" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'twentyeleven' ); ?>" />
 	</form>
